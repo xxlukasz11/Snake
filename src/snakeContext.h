4dba311bd57b4a58dc37b04a586fd74be0835553
Lukasz Bozek <xxlukasz11@gmail.com>
2022-10-18 20:39:32 +0200

    Add snake context

---
+++ src/snakeContext.h
@@ -0,0 +1,36 @@
+#ifndef SRC_SNAKECONTEXT_H_
+#define SRC_SNAKECONTEXT_H_
+
+#include <deque>
+#include <allegro5/allegro_color.h>
+
+struct Position {
+	int x;
+	int y;
+};
+
+struct SpeedVector {
+	int x;
+	int y;
+};
+
+class SnakeContext {
+public:
+	using Body = std::deque<Position>;
+
+	const ALLEGRO_COLOR& getHeadColor() const;
+	const ALLEGRO_COLOR& getBodyColor() const;
+	const Body& getBody() const;
+	const SpeedVector& getSpeed() const;
+
+	void setSpeed(const SpeedVector& newSpeed);
+	void appendHeadSegment(const Position& headPosition);
+	void eraseTailSegment();
+
+private:
+	SpeedVector speed { 0, 0 };
+	Body body;
+};
+
+
+#endif /* SRC_SNAKECONTEXT_H_ */
