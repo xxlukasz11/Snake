f480382251d6844d6665dd34ef221b3999fe26dd
Lukasz Bozek <xxlukasz11@gmail.com>
2022-10-20 23:24:39 +0200

    remove escape handling from state base

--- src/stateBase.cpp
+++ src/stateBase.cpp
@@ -5,14 +5,8 @@ StateBase::StateBase(StateMachine& stateMachine) :
 }
 
 void StateBase::handleEvent(const ALLEGRO_EVENT& event) {
-	switch (event.type) {
-	case ALLEGRO_EVENT_DISPLAY_CLOSE:
+	if (event.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
 		exitGame();
-		return;
-	case ALLEGRO_EVENT_KEY_UP:
-		if (event.keyboard.keycode == ALLEGRO_KEY_ESCAPE) {
-			exitGame();
-		}
 		return;
 	}
 	handleStateEvent(event);
