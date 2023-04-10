7e9262517fd9c0f0b9474fa448f74dbc31ecf2e5
Lukasz Bozek <xxlukasz11@gmail.com>
2023-04-10 22:00:24 +0200

    Use vector from allegrocpp

--- src/utils.h
+++ src/utils.h
@@ -1,18 +1,13 @@
 #ifndef SRC_UTILS_H_
 #define SRC_UTILS_H_
 
-struct Vector2D {
-	int x;
-	int y;
-};
+#include "allegro-cpp/vec.h"
+
+using Vector2D = allegrocpp::IntVec;
 
 struct Area {
 	Vector2D topLeft;
 	Vector2D bottomRight;
 };
-
-inline bool operator==(const Vector2D& left, const Vector2D& right) {
-	return left.x == right.x && left.y == right.y;
-}
 
 #endif /* SRC_UTILS_H_ */
